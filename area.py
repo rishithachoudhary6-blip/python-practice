d2acaefa4c4df0347d5246efcba9d904754e907d
rishithachoudhary6-blip <rishithachoudhary6@gmail.com>
2025-12-13 12:50:29 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,9 @@
+
+#write a python program find the area and circumference of rectengle?
+
+length=int(input("length of the rectangle:"))
+breadth=int(input("breadth of the rectangle:"))
+area=length*breadth
+circumference=2*(length+breadth)
+print("area of rectangle is",area)
+print("circumference of rectangle is",circumference)
